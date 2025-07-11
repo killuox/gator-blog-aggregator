26badb58090a1714791ae7afb31cc1a4b17b821f
Antonin Arsenault <anto94@live.ca>
2025-07-11 10:05:39 -0400

    Add users table

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
