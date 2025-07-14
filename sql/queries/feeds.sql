9d00f44bc340d80c6a72885d8f7572470a17d406
Antonin Arsenault <anto94@live.ca>
2025-07-14 12:24:29 -0400

    Add last_fetch_at for feeds and some queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -17,3 +17,15 @@ INNER JOIN users ON users.id = feeds.user_id;
 -- name: GetFeedByUrl :one
 SELECT * FROM feeds
 WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $1, updated_at = $1
+WHERE feeds.id = $2;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY
+    last_fetched_at ASC NULLS FIRST,
+    last_fetched_at ASC
+LIMIT 1;
